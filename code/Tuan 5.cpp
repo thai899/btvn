974762db46cf624b4200ccf71c708f5ca524ef66
Thai <trinhhoangthai2005@gmail.com>
2024-08-06 20:07:01 +0700

    btvn tuan 5 bai 10

--- code/Tuan 5.cpp
+++ code/Tuan 5.cpp
@@ -31,6 +31,35 @@ double S_khuDeQuy(int n) {
     }
     return sum;
 }
+unsigned long long fibonacci(int k) {
+    if (k <= 2) {
+        return 1;
+    }
+    unsigned long long a = 1, b = 1, c;
+    for (int i = 3; i <= k; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
+
+void xuatFibonacciTrongDoan(int m, int n) {
+    int k = 1;
+    unsigned long long fib = fibonacci(k);
+
+    while (fib < m) {
+        k++;
+        fib = fibonacci(k);
+    }
+
+    while (fib <= n) {
+        printf("%llu ", fib);
+        k++;
+        fib = fibonacci(k);
+    }
+    printf("\n");
+}
 
 
 int main()
@@ -64,7 +93,23 @@ int main()
             printf("S(n) tinh theo cach khu de quy: %lf\n", result_khuDeQuy);
 
         }break;
-       
+        case 2:
+        {
+            int m, n;
+
+            printf("Nhap m: ");
+            scanf_s("%d", &m);
+            printf("Nhap n: ");
+            scanf_s("%d", &n);
+
+            if (m <= 0 || n <= 0 || m > n) {
+                printf("Vui long nhap m va n sao cho m n va m n phai lon hon 0.\n");
+                return 1;
+            }
+
+            printf("Day so Fibonacci thuoc doan [%d, %d]:\n", m, n);
+            xuatFibonacciTrongDoan(m, n);
+        }break;
         }
     } while (lc < 7);
     return 0;
